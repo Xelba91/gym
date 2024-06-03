713c090804ac268e2d0f03e847cda3afcb0dc28d
Xelba91 <a_lbert_o@hotmail.com>
2024-06-03 14:15:25 +0200

    final

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,12 @@
+<x-app-layout>
+    <div class="bg-cover bg-center" >
+        <div class="flex justify-center items-center h-screen">
+            <div class="max-w-4xl px-6 py-12 bg-white bg-opacity-80 rounded-lg shadow-lg text-center">
+                <h2 class="text-3xl font-bold text-gray-900 dark:text-white mb-4">Benvenuto nella palestra Xelba</h2>
+                <p class="text-lg text-gray-700 dark:text-black-300">
+                    La Palestra Xelba è qui per supportarti nel tuo percorso di fitness. Con una vasta gamma di corsi e attività adatti a tutti i livelli, hai tutte le risorse necessarie per raggiungere i tuoi obiettivi. Dai un'occhiata ai nostri programmi e inizia oggi stesso il tuo viaggio verso una vita più sana!
+                </p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
